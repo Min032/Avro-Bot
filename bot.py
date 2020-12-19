5beeb0e94567ce31ba4d2872b3f6205563ca88ae
Min032 <asocial.bastard3@gmail.com>
2020-12-19 17:02:09 +0100

    Got back on server credentials

--- bot.py
+++ bot.py
@@ -575,7 +575,7 @@ def list_comments(update, context):
             cursor.close()
             conn.close()
             print_log("PostgreSQL connection is closed.")
-            
+
 
 def kraljevo(update, context):
     global kraljevo_path
@@ -743,8 +743,8 @@ PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(token=TOKEN, use_context=True)
 job_queuer = updater.job_queue
 
-# updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-# updater.bot.setWebhook('https://avro-bot.herokuapp.com/' + TOKEN)
+updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+updater.bot.setWebhook('https://avro-bot.herokuapp.com/' + TOKEN)
 
 dispatcher = updater.dispatcher
 
